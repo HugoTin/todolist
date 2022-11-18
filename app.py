c45ef8ac4b1cc608dab594ec1fe9a3f3ee2174c3
Victor Tin <hugo.tin@hotmail.com>
2022-11-18 20:24:06 +0000

    Start Code

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("Hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #template/home.html
+    return render_template('home.html')
+
+@app.route("/test")
+def test():
+    #template/home.html
+    return render_template('test.html')
+
+@app.route("/bye")
+def bye():
+    return 'Bye'
+
+app.run(debug = True)
