6e93811913b97a39372d866b9c8d68a11821c769
Victor Tin <hugo.tin@hotmail.com>
2022-11-18 21:20:19 +0000

    NICE

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     #template/home.html
-    return render_template('home.html')
+    with open("app.json") as f:
+        tasks = json.load(f)
+    return render_template('home.html', tasks=tasks)
 
-@app.route("/test")
-def test():
-    #template/home.html
-    return render_template('test.html')
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.form['name']
+    task = {
+        "name" :  name,
+        "finished": False
+    }
+    with open("app.json") as f:
+        tasks = json.load(f)
 
-@app.route("/bye")
-def bye():
-    return 'Bye'
+    tasks.append(task)
+
+    with open("app.json", 'w') as f:
+        json.dump(tasks, f, indent = 2)
+    
+    return home()
+
 
 app.run(debug = True)
